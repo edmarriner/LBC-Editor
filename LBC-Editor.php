71808b7fa87fe7b5b6d2150e4524823ed2cdc14e
edward marriner <edmarriner@gmail.com>
2015-03-05 21:23:44 +0000

    New plugin main file

---
+++ LBC-Editor.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Plugin Name: Name of the plugin, must be unique.
+ * Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+ * Description: A brief description of the plugin.
+ * Version: The plugin's version number. Example: 1.0.0
+ * Author: Name of the plugin author
+ * Author URI: http://URI_Of_The_Plugin_Author
+ * Text Domain: Optional. Plugin's text domain for localization. Example: mytextdomain
+ * Domain Path: Optional. Plugin's relative directory path to .mo files. Example: /locale/
+ * Network: Optional. Whether the plugin can only be activated network wide. Example: true
+ * License: A short license name. Example: GPL2
+ */
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+/** Step 2 (from text above). */
+add_action( 'admin_menu', 'my_plugin_menu' );
+
+/** Step 1. */
+function my_plugin_menu() {
+	add_options_page( 'My Plugin Options', 'My Plugin', 'manage_options', 'my-unique-identifier', 'my_plugin_options' );
+}
+
+/** Step 3. */
+function my_plugin_options() {
+	if ( !current_user_can( 'manage_options' ) )  {
+		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+	}
+	echo '<div class="wrap">';
+	echo '<p>Here is where the form would go if I actually had options.</p>';
+	echo '</div>';
+}
