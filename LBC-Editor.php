4dcdb1a1adaef6af8e7f6beaaf2f7e8d3f771559
edward marriner <edmarriner@gmail.com>
2015-03-08 18:41:24 +0000

    Adding menu link

--- LBC-Editor.php
+++ LBC-Editor.php
@@ -14,20 +14,18 @@
 
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
-/** Step 2 (from text above). */
-add_action( 'admin_menu', 'my_plugin_menu' );
+add_action( 'admin_menu', 'register_my_custom_menu_page' );
 
-/** Step 1. */
-function my_plugin_menu() {
-	add_options_page( 'My Plugin Options', 'My Plugin', 'manage_options', 'my-unique-identifier', 'my_plugin_options' );
-}
+function register_my_custom_menu_page() {
 
-/** Step 3. */
-function my_plugin_options() {
-	if ( !current_user_can( 'manage_options' ) )  {
-		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
-	}
-	echo '<div class="wrap">';
-	echo '<p>Here is where the form would go if I actually had options.</p>';
-	echo '</div>';
+	add_menu_page(
+		'custom menu title',
+		'custom menu',
+		'manage_options',
+		'myplugin/myplugin-admin.php',
+		'',
+		plugins_url( 'myplugin/images/icon.png' ),
+		6
+	);
+
 }
